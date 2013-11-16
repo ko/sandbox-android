e286a6c5c337df5ac7eee398ffefb62dc0cbf162
ko <git@yaksok.net>
2013-11-15 23:20:47 -0800

    reset it all! (oauth,spreadsheet)

--- src/com/relurori/sandbox/spreadsheetapi/SpreadsheetDemoActivity.java
+++ src/com/relurori/sandbox/spreadsheetapi/SpreadsheetDemoActivity.java
@@ -1,8 +1,5 @@
 package com.relurori.sandbox.spreadsheetapi;
 
-import com.google.gdata.client.spreadsheet.SpreadsheetService;
-import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
-import com.google.gdata.data.spreadsheet.SpreadsheetFeed;
 import com.relurori.sandbox.R;
 
 import android.app.Activity;
@@ -23,24 +20,6 @@ public class SpreadsheetDemoActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_spreadsheet_demo);
 		
-		SpreadsheetService service;
-		SpreadsheetFeed feed;
-		int i = 0;
-		try {
-		    SpreadsheetEntry spreadsheet;
-		    service = new SpreadsheetService("Spreadsheet");
-		    service.setProtocolVersion(SpreadsheetService.Versions.V3);
-		    service.setUserCredentials("username", "password");//permission required to add in Manifest
-		    URL metafeedUrl = new URL("https://spreadsheets.google.com/feeds/spreadsheets/private/full");
-		    feed = service.getFeed(metafeedUrl, SpreadsheetFeed.class);
-
-		    List<SpreadsheetEntry> spreadsheets = feed.getEntries();
-		    if (spreadsheets.size() > 0) {
-		        spreadsheet = spreadsheets.get(i);//Get your Spreadsheet
-		        Log.d(TAG,"" + spreadsheet.getTitle());
-		   }
-		} catch (Exception e) {
-		    e.printStackTrace();
-		}
+		
 	}
 }
