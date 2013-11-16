5b174a3338386f199665a6ab534ff6fa0ab50914
ko <git@yaksok.net>
2013-11-16 00:12:39 -0800

    empty spreadsheet again!

--- src/com/relurori/sandbox/spreadsheetapi/SpreadsheetDemoActivity.java
+++ src/com/relurori/sandbox/spreadsheetapi/SpreadsheetDemoActivity.java
@@ -3,23 +3,26 @@ package com.relurori.sandbox.spreadsheetapi;
 import com.relurori.sandbox.R;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import java.io.IOException;
 import java.net.*;
 import java.util.*;
 
+public class SpreadsheetDemoActivity extends Activity  {
 
-public class SpreadsheetDemoActivity extends Activity {
+	private static final String TAG = SpreadsheetDemoActivity.class
+			.getSimpleName();
 
-	private static final String TAG = SpreadsheetDemoActivity.class.getSimpleName();
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_spreadsheet_demo);
-		
-		
+
 	}
 }
