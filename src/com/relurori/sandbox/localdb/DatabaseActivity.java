35c65e3ea6f0c6b40f823aa789add06d63978beb
ko <git@yaksok.net>
2013-11-18 23:13:46 -0800

    Need to read on SQLiteOpenHelper...

---
+++ src/com/relurori/sandbox/localdb/DatabaseActivity.java
@@ -0,0 +1,14 @@
+package com.relurori.sandbox.localdb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DatabaseActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		
+		
+	}
+}
