ec9d9b6e241f9ff0c429a7212ac8d49363f4fd4a
ko <git@yaksok.net>
2013-11-20 00:11:21 -0800

    start looking at crop. maybe use library?

--- src/com/relurori/sandbox/camera/crop/CameraCropActivity.java
+++ src/com/relurori/sandbox/camera/crop/CameraCropActivity.java
@@ -1,7 +1,22 @@
 package com.relurori.sandbox.camera.crop;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 
 public class CameraCropActivity extends Activity {
 
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		
+		cropIt();
+	}
+
+	private void cropIt() {
+		Intent intent = new Intent("com.android.camera.action.CROP");
+	    intent.setType("image/*");
+	    
+
+	}
 }
